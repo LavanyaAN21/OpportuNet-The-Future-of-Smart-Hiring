dabca0783240b8f3919e25ea28f170a31e37a980
Lavanya A N <lavanyaan79@gmail.com>
2025-05-07 20:54:44 +0530

    Update app.py to host

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ from pythonfiles.recruiterslogin import *
 from pythonfiles.students_login import *
 from pythonfiles.Students_register import register_student
 from flask_cors import CORS
+import os
+
 
 app = Flask(__name__)
 app.secret_key = '2345'
@@ -203,5 +205,7 @@ def api_report_data():
     return jsonify(report_data) 
 # ++++++++++++++++++++++++++++++++++++++ END OF REPORT PAGE +++++++++++++++++++++++++++++
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port,debug=True)
